d002f0a722be577ef1cf6dcb370cf41c9f562587
rimmerz <zrimmer@regatta.com>
2016-11-02 11:30:45 +0000

    added code for show/hide LHN

--- hawkshead/js/hawkshead.js
+++ hawkshead/js/hawkshead.js
@@ -84,4 +84,29 @@ $j(document).ready(function($) {
     }).resize();
   }, 9000);
 
+  /* Left Hand Nav show/hide */
+  if ($j('#narrow-by-list').length) {
+
+    $j('#narrow-by-list ol').each(function(){
+      var numberOfSolrItems = $j(this).children().length;
+
+      if (numberOfSolrItems > 5) {
+        $j(this).children('li:nth-child(n+6)').css('display', 'none');
+        $j(this).append('<p class="lhn-show-more-less"><a class="lhn-show-more-link">Show more +</a><a class="lhn-show-less-link">Show less -</a></p>');
+
+        $j('.lhn-show-more-link').on('click', function(){
+          $j(this).closest('ol').children('li:nth-child(n+6)').fadeIn(400);
+          $j(this).css('display', 'none');
+          $j(this).next('.lhn-show-less-link').css('display', 'block');
+        });
+
+        $j('.lhn-show-less-link').on('click', function(){
+          $j(this).closest('ol').children('li:nth-child(n+6)').fadeOut(400);
+          $j(this).css('display', 'none');
+          $j(this).prev('.lhn-show-more-link').css('display', 'block');
+        });
+      }
+    });  
+  }
+
 });
