615b198d272f1486766a0982b90aa46ca6c95b3d
David Adams <dadams@regatta.com>
2016-12-08 09:31:29 +0000

    Hawks New Theme - final tweaks

--- hawkshead/js/hawkshead-nt.js
+++ hawkshead/js/hawkshead-nt.js
@@ -7,25 +7,6 @@
 
 $j(document).ready(function($) {
 
-
-  //Nasty hack to make AJAX cart work
-
-  try {
-
-    if ($j(AmAjaxObj).length) {
-
-      $j('.btn-cart').on('click', function(){
-
-        $j(this).removeAttr('onclick');
-        
-      });
-    }
-  }
-  catch (err) {
-    console.log('AJAX basket failed to load');
-    ga('send', 'event', 'AJAX Cart Fail', 'load', err);
-  }
-
   $j('.block-title').click(function() {
     $j('#narrow-by-list').toggleClass('no-display');
     $j('.block-title').toggleClass('block-title--open');
